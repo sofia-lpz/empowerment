535dc25b22630658e3d641c9a5bb28d7af241956
sofia-lpz <a01028251@tec.mx>
2024-12-05 19:43:38 -0600

    takes both actions

--- combined.py
+++ combined.py
@@ -16,8 +16,10 @@ num_cols = len(obstacle_map[0])
 q_table = np.loadtxt('qtable.txt').reshape(len(obstacle_map), len(obstacle_map[0]), env.action_space.n)
 eq_table = np.loadtxt('eqtable.txt').reshape(len(obstacle_map), len(obstacle_map[0]), env.action_space.n)
 
-def choose_action(state):
-    return np.argmax(q_table[state[0], state[1]])
+def choose_combined_actions(state):
+    q_action = np.argmax(q_table[state[0], state[1]])
+    emp_action = np.argmax(eq_table[state[0], state[1]])
+    return q_action, emp_action
 
 def convert_obs_to_tuple(obs):
     x = obs // num_cols # integer division
@@ -37,12 +39,20 @@ done = False
 total_reward = 0
 
 while not done:
-    action = choose_action(state)
-    obs, reward, done, _, info = env_test.step(action)
+    # Get both actions
+    q_action, emp_action = choose_combined_actions(state)
+    
+    # Take Q-learning action first
+    obs, reward1, done1, _, info = env_test.step(q_action)
+    if not done1:
+        # Take empowerment action second
+        obs, reward2, done, _, info = env_test.step(emp_action)
+        total_reward += (reward1 + reward2)
+    else:
+        total_reward += reward1
+        done = True
 
     state = convert_obs_to_tuple(obs)
-    total_reward += reward
-    
     env_test.render()
 
 print(f"Total Reward: {total_reward:.2f}")
